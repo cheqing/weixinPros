4f39ce1100abd88cd91992097f0beaac223de4ea
BoyceLee <cheqinglby@163.com>
2017-12-08 13:49:06 +0800

    update
    添加和修改了一些新的模块

--- 微信练手项目/wx-smpro-test/pages/index/index.js
+++ 微信练手项目/wx-smpro-test/pages/index/index.js
@@ -6,22 +6,53 @@ Page({
    * 页面的初始数据
    */
   data: {
-    // text:"这是一个页面"
-    actionSheetHidden: true,
-    actionSheetItems: ['item1', 'item2', 'item3']
+    swiperImgs: [
+      '../imgs/swiper1.jpg',
+      '../imgs/swiper2.jpg',
+      '../imgs/swiper3.jpg'
+    ],
+    menuImgs: [
+      { name: '美食', url: '../imgs/food.ico' },
+      { name: '旅游', url: '../imgs/travel.ico' },
+      { name: '住宿', url: '../imgs/hotel.ico' },
+      { name: '电话', url: '../imgs/phone.ico' },
+      { name: '商场', url: '../imgs/shop.ico' },
+      { name: '银行', url: '../imgs/bank.ico' },
+      { name: '药店', url: '../imgs/medical.ico' },
+      { name: '电影', url: '../imgs/movie.ico' }
+    ],
+    shop_info: [
+      {
+        info_img:'/pages/imgs/hanbao.ico',
+        info_title: '肯德基汉堡(胡同店)\r\n',
+        info_msg: '30分钟送达\r\n起送价￥20|配送费￥5\r\n',
+        info_price: '满50减10，满100减25',
+        info_nums: '月售23487',
+      },
+      {
+        info_img: '../imgs/xingbake.ico',
+        info_title: '星巴克奶茶(胡同店)\r\n',
+        info_msg: '20分钟送达\r\n起送价￥15|配送费￥3\r\n',
+        info_price: '满20减5，满25减7',
+        info_nums: '月售568',
+      },
+      {
+        info_img: '../imgs/fastfood.ico',
+        info_title: '田老师(安贞门店)\r\n',
+        info_msg: '40分钟送达\r\n起送价￥20|配送费￥7\r\n',
+        info_price: '满30减5，满50减7',
+        info_nums: '月售6589',
+      },
+      {
+        info_img: '../imgs/noodles.ico',
+        info_title: '李先生牛肉面(北土城店)\r\n',
+        info_msg: '25分钟送达\r\n起送价￥25|配送费￥6\r\n',
+        info_price: '满30减3，满50减8',
+        info_nums: '月售1426',
+      }
+    ]
   },
-  listenerButton: function () {
-    this.setData({
-      //取反
-      actionSheetHidden: !this.data.actionSheetHidden
-    });
-  },
-
-  listenerActionSheet: function () {
-    this.setData({
-      actionSheetHidden: !this.data.actionSheetHidden
-    })
-  },
+  
   
   /**
    * 生命周期函数--监听页面加载
