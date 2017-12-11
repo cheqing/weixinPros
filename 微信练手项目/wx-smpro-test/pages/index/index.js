61069aa25085c922a032bfbaa64d3e17acecf68a
BoyceLee <cheqinglby@163.com>
2017-12-11 18:12:40 +0800

    icon--png
    小程序在手机端是不支持ico图标的

--- 微信练手项目/wx-smpro-test/pages/index/index.js
+++ 微信练手项目/wx-smpro-test/pages/index/index.js
@@ -12,39 +12,39 @@ Page({
       '../imgs/swiper3.jpg'
     ],
     menuImgs: [
-      { name: '美食', url: '../imgs/food.ico' },
-      { name: '旅游', url: '../imgs/travel.ico' },
-      { name: '住宿', url: '../imgs/hotel.ico' },
-      { name: '电话', url: '../imgs/phone.ico' },
-      { name: '商场', url: '../imgs/shop.ico' },
-      { name: '银行', url: '../imgs/bank.ico' },
-      { name: '药店', url: '../imgs/medical.ico' },
-      { name: '电影', url: '../imgs/movie.ico' }
+      { name: '美食', url: '../imgs/food.png' },
+      { name: '旅游', url: '../imgs/travel.png' },
+      { name: '住宿', url: '../imgs/shop.png' },
+      { name: '电话', url: '../imgs/phone.png' },
+      { name: '商场', url: '../imgs/shop.png' },
+      { name: '银行', url: '../imgs/bank.png' },
+      { name: '药店', url: '../imgs/phone.png' },
+      { name: '电影', url: '../imgs/food.png' }
     ],
     shop_info: [
       {
-        info_img:'/pages/imgs/hanbao.ico',
+        info_img:'/pages/imgs/hanbao.png',
         info_title: '肯德基汉堡(胡同店)\r\n',
         info_msg: '30分钟送达\r\n起送价￥20|配送费￥5\r\n',
         info_price: '满50减10，满100减25',
         info_nums: '月售23487',
       },
       {
-        info_img: '../imgs/xingbake.ico',
+        info_img: '../imgs/xingbake.png',
         info_title: '星巴克奶茶(胡同店)\r\n',
         info_msg: '20分钟送达\r\n起送价￥15|配送费￥3\r\n',
         info_price: '满20减5，满25减7',
         info_nums: '月售568',
       },
       {
-        info_img: '../imgs/fastfood.ico',
+        info_img: '../imgs/food.png',
         info_title: '田老师(安贞门店)\r\n',
         info_msg: '40分钟送达\r\n起送价￥20|配送费￥7\r\n',
         info_price: '满30减5，满50减7',
         info_nums: '月售6589',
       },
       {
-        info_img: '../imgs/noodles.ico',
+        info_img: '../imgs/noodles.png',
         info_title: '李先生牛肉面(北土城店)\r\n',
         info_msg: '25分钟送达\r\n起送价￥25|配送费￥6\r\n',
         info_price: '满30减3，满50减8',
