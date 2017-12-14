f6288496285db54c25f2d91cf01cbe3ffed19811
BoyceLee <cheqinglby@163.com>
2017-12-14 17:49:43 +0800

    编写首页以及排序页
    编写首页以及排序页

--- 微信练手项目/wx-smpro-test/pages/goods/goods.js
+++ 微信练手项目/wx-smpro-test/pages/goods/goods.js
@@ -59,20 +59,103 @@ Page({
       ]
     },
     site: '燕莎盛世大厦',
-    actionSheetHidden: true,
-    actionSheetItems: ['item1', 'item2', 'item3']
+    shop_list: [
+      {
+        shop_img: 'http://i4.piimg.com/601998/23f361491b45ddf2.jpg',
+        shop_title: '精武鸭脖(胡同店)\r\n',
+        shop_msg: '30分钟送达\r\n起送价￥20|配送费￥5\r\n',
+        shop_price: '满50减10，满100减25',
+        shop_nums: '月售23487',
+      },
+      {
+        shop_img: 'http://i2.kiimg.com/601998/a955867016875a41.jpg',
+        shop_title: '御膳房\r\n',
+        shop_msg: '20分钟送达\r\n起送价￥15|配送费￥3\r\n',
+        shop_price: '满20减5，满25减7',
+        shop_nums: '月售568',
+      },
+      {
+        shop_img: 'http://i4.piimg.com/601998/9ce47f2f19d7717d.jpg',
+        shop_title: '韩式炸鸡啤酒屋\r\n',
+        shop_msg: '40分钟送达\r\n起送价￥20|配送费￥7\r\n',
+        shop_price: '满30减5，满50减7',
+        shop_nums: '月售6589',
+      },
+      {
+        shop_img: 'http://i4.piimg.com/601998/da9e00c0bccd6fb0.jpg',
+        shop_title: '榴芒先生(北土城店)\r\n',
+        shop_msg: '25分钟送达\r\n起送价￥25|配送费￥6\r\n',
+        shop_price: '满30减3，满50减8',
+        shop_nums: '月售1426',
+      }
+    ],
+    sortList:[
+      { name: '综合排序', img: '' },
+      { name: '速度最快', img: '' },
+      { name: '评分最高', img: '' },
+      { name: '起送价最低', img: '' },
+      { name: '配送费最低', img: '' },
+    ],
+    sortSelected: '综合排序',
+    maskSortHidden: true,
+    maskFilterHidden: true,
+    sortActive:0,
+    shopFeatureList:[
+      { name: '免费配送', img: '' },
+      { name: '0元起送', img: '' },
+      { name: '新商家', img: '' },
+      { name: '品牌商家', img: '' },
+      { name: '超时免赔', img: '' },
+    ],
+    featureSelected: [],
+    featureItemActive:0
   },
-  listenerButton: function () {
+  // 点击排序，弹出模态层
+  onSortShow: function(e){
+    //隐藏筛选模态层
+    // this.onFilterHide()
+    // 显示排序模态层
     this.setData({
-      //取反
-      actionSheetHidden: !this.data.actionSheetHidden
+      maskSortHidden: false
+    })
+  },
+  // 点击排序，隐藏模态层
+  onSortHide: function(e){
+    this.setData({
+      maskSortHidden: true
+    })
+  },
+  // 点击筛选，弹出筛选模态层
+  onFilterShow: function(e){
+    // 隐藏排序模态层
+    // this.onSortHide()
+    // 显示筛选模态层
+    this.setData({
+      maskFilterHidden: false
+    })
+  },
+  // 点击筛选，隐藏筛选模态层
+  onFilterHide: function (e) {
+    this.setData({
+      maskFilterHidden: true
+    })
+  },
+  // 选中排序方式后，赋值并隐藏模态层
+  onSortSelect: function(e){
+    this.onSortHide(e);
+    //为选中的排序规则赋值,后期这里会请求后台接口
+    this.setData({
+      sortSelected: this.data.sortList[e.currentTarget.dataset.index].name
     });
   },
-
-  listenerActionSheet: function () {
+  // 选中排序事件 销量最高/距离最近
+  onTapTag: function(e){
     this.setData({
-      actionSheetHidden: !this.data.actionSheetHidden
+      sortActive: e.currentTarget.dataset.index
     })
+    // 选中则关闭模态框
+    this.onSortHide()
+    this.onFilterHide()
   },
   
   /**
