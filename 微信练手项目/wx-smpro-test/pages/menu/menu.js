2947d92e74465eac6d229dceaf098673ea81e054
BoyceLee <cheqinglby@163.com>
2017-12-21 19:55:36 +0800

    update

--- 微信练手项目/wx-smpro-test/pages/menu/menu.js
+++ 微信练手项目/wx-smpro-test/pages/menu/menu.js
@@ -5,6 +5,7 @@ Page({
     tabs: ["点菜", "评价", "商家"],  // 导航栏选项
     activeIndex: 0, // 顶部导航栏选中项
     currMenu: 0,  //左侧菜单栏选中项
+    // foodNums: 0,
     // sliderOffset: 0,
     // sliderLeft: 0,
     menu: [
@@ -12,37 +13,37 @@ Page({
         id: 11,
         typeName: '快餐',
         content: [
-          { name: "炸鸡", img: "http://i2.kiimg.com/601998/a955867016875a41.jpg", sales:22,  rating: 3, price: 15, nums: 10},
-          { name: "汉堡", img: "http://i4.piimg.com/601998/9ce47f2f19d7717d.jpg", sales: 22, rating: 3, price: 15, nums: 10 },
-          { name: "鸡翅", img: "http://i4.piimg.com/601998/a88338a6d392a569.jpg", sales: 22, rating: 3, price: 15, nums: 10 },
-          { name: "薯条", img: "http://i4.piimg.com/601998/473847a250bb0186.jpg", sales: 22, rating: 3, price: 15, nums: 10 }
+          { name: "炸鸡", img: "http://i2.kiimg.com/601998/a955867016875a41.jpg", sales:22,  rating: 3, price: 15, nums: 0},
+          { name: "汉堡", img: "http://i4.piimg.com/601998/9ce47f2f19d7717d.jpg", sales: 22, rating: 3, price: 15, nums: 0 },
+          { name: "鸡翅", img: "http://i4.piimg.com/601998/a88338a6d392a569.jpg", sales: 22, rating: 3, price: 15, nums: 0 },
+          { name: "薯条", img: "http://i4.piimg.com/601998/473847a250bb0186.jpg", sales: 22, rating: 3, price: 15, nums: 0 }
         ]
       },
       {
         id: 12,
         typeName: '盖浇饭',
         content: [
-          { name: "土豆牛肉盖浇饭", img: "http://i4.piimg.com/601998/a014d6160fd7b504.jpg", sales: 22, rating: 3, price: 15, nums: 10 },
-          { name: "番茄炒蛋盖浇饭", img: "http://i4.piimg.com/601998/da9e00c0bccd6fb0.jpg", sales: 22, rating: 3, price: 15, nums: 10 },
-          { name: "鸡翅", img: "http://i4.piimg.com/601998/a88338a6d392a569.jpg", sales: 22, rating: 3, price: 15, nums: 10 },
-          { name: "薯条", img: "http://i4.piimg.com/601998/473847a250bb0186.jpg", sales: 22, rating: 3, price: 15, nums: 10 }
+          { name: "土豆牛肉盖浇饭", img: "http://i4.piimg.com/601998/a014d6160fd7b504.jpg", sales: 22, rating: 3, price: 15, nums: 0 },
+          { name: "番茄炒蛋盖浇饭", img: "http://i4.piimg.com/601998/da9e00c0bccd6fb0.jpg", sales: 22, rating: 3, price: 15, nums: 0 },
+          { name: "鸡翅", img: "http://i4.piimg.com/601998/a88338a6d392a569.jpg", sales: 22, rating: 3, price: 15, nums: 0 },
+          { name: "薯条", img: "http://i4.piimg.com/601998/473847a250bb0186.jpg", sales: 22, rating: 3, price: 15, nums: 0 }
         ]
       },
       {
         id: 13,
         typeName: '养生粥',
         content: [
-          { name: "桂圆莲子", img: "http://i4.piimg.com/601998/a88338a6d392a569.jpg", sales: 22, rating: 3, price: 15, nums: 10 },
-          { name: "皮蛋瘦肉粥", img: "http://i4.piimg.com/601998/da9e00c0bccd6fb0.jpg", sales: 22, rating: 3, price: 15, nums: 10 },
-          { name: "鸡翅", img: "http://i4.piimg.com/601998/a88338a6d392a569.jpg", sales: 22, rating: 3, price: 15, nums: 10 },
-          { name: "薯条", img: "http://i4.piimg.com/601998/473847a250bb0186.jpg", sales: 22, rating: 3, price: 15, nums: 10 }
+          { name: "桂圆莲子", img: "http://i4.piimg.com/601998/a88338a6d392a569.jpg", sales: 22, rating: 3, price: 15, nums: 0 },
+          { name: "皮蛋瘦肉粥", img: "http://i4.piimg.com/601998/da9e00c0bccd6fb0.jpg", sales: 22, rating: 3, price: 15, nums: 0 },
+          { name: "鸡翅", img: "http://i4.piimg.com/601998/a88338a6d392a569.jpg", sales: 22, rating: 3, price: 15, nums: 0 },
+          { name: "薯条", img: "http://i4.piimg.com/601998/473847a250bb0186.jpg", sales: 22, rating: 3, price: 15, nums: 0 }
         ]
       },
       {
         id: 14,
         typeName: '小吃',
         content: [
-          { name: "肉夹馍", img: "http://i4.piimg.com/601998/473847a250bb0186.jpg", sales: 22, rating: 3, price: 15, nums: 10 }
+          { name: "肉夹馍", img: "http://i4.piimg.com/601998/473847a250bb0186.jpg", sales: 22, rating: 3, price: 15, nums: 0 }
         ]
       }
     ],
@@ -62,6 +63,28 @@ Page({
     // 获取点击的菜单索引并进行赋值保存
     this.setData({
       currMenu: e.currentTarget.dataset.index
+    });
+  },
+
+  // 物品减少事件
+  onReduce: function(e){
+    // 没点击一次将物品数量-1，当减到0的时候需要隐藏掉减号按钮
+    var info = this.data.menu;
+    if (info[this.data.currMenu].content[e.currentTarget.dataset.index].nums > 0){
+      info[this.data.currMenu].content[e.currentTarget.dataset.index].nums--
+      this.setData({
+        menu: info
+      });
+    }
+  },
+
+  // 物品增加事件
+  onAdd: function(e){
+    // 在实际新增的时候还要判断新增数量不能超过库存的物品量
+    var info = this.data.menu
+    info[this.data.currMenu].content[e.currentTarget.dataset.index].nums++
+    this.setData({
+      menu: info
     });
   },
 
